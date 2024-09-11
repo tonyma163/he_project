991d5e6054802a11e976e26092505ca2e4504053
tonyma163 <mamanto163@gmail.com>
2024-09-12 00:51:32 +0800

    addition & multiplication testing

--- test.cu
+++ test.cu
@@ -21,7 +21,7 @@ int main() {
 	
 	size_t poly_modulus_degree = 8192;
 	parms.set_poly_modulus_degree(poly_modulus_degree);
-	parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 60}));
+	parms.set_coeff_modulus(CoeffModulus::Create(poly_modulus_degree, {60, 40, 40, 40, 60}));
 	
 	PhantomContext context(parms);
 
@@ -29,6 +29,9 @@ int main() {
 	PhantomSecretKey secret_key(context);
 	PhantomPublicKey public_key = secret_key.gen_publickey(context);
 	
+	// Create relinear key for post-multiplication
+	PhantomRelinKey relin_keys = secret_key.gen_relinkey(context);
+
 	// Encoder & encryptor setup
 	PhantomCKKSEncoder encoder(context);
 	double scale = pow(2.0, 40); // Scale factor
@@ -43,9 +46,41 @@ int main() {
 	PhantomCiphertext ciphertext;
 	public_key.encrypt_asymmetric(context, plaintext, ciphertext);
 
+	// Multiplication - origin * origin
+	PhantomCiphertext product_ciphertext = multiply(context, ciphertext, ciphertext);
+	relinearize_inplace(context, product_ciphertext, relin_keys);
+	rescale_to_next_inplace(context, product_ciphertext);
+
+	/*
+	// Addition - product * product
+	add_inplace(context, product_ciphertext, ciphertext); // add itself
+	*/
+
+	/*
+	// Addition - product * origin
+	product_ciphertext.set_scale(scale);
+	mod_switch_to_next_inplace(context, ciphertext);
+	add_inplace(context, product_ciphertext, ciphertext);
+	*/
+
+
+	// Multiplication - product * origin
+	product_ciphertext.set_scale(scale);
+	mod_switch_to_next_inplace(context, ciphertext);
+	PhantomCiphertext product_ciphertext2 = multiply(context, product_ciphertext, ciphertext);
+	relinearize_inplace(context, product_ciphertext2, relin_keys);
+	rescale_to_next_inplace(context, product_ciphertext2);
+
+	// Multiplication - product * origin
+	product_ciphertext2.set_scale(scale);
+	mod_switch_to_next_inplace(context, ciphertext);
+	PhantomCiphertext product_ciphertext3 = multiply(context, product_ciphertext2, ciphertext);
+	relinearize_inplace(context, product_ciphertext3, relin_keys);
+	rescale_to_next_inplace(context, product_ciphertext3);
+
 	// Decrypt and decode ciphertext
 	PhantomPlaintext decrypted_plaintext;
-	secret_key.decrypt(context, ciphertext, decrypted_plaintext);
+	secret_key.decrypt(context, product_ciphertext3, decrypted_plaintext);
 	vector<double> output_vector;
 	encoder.decode(context, decrypted_plaintext, output_vector);
 
